415498bcfe9be22c1b5d7cfb05ee600c7216cf1d
Ignacio Jordan <ignaciobjordan@gmail.com>
2023-02-08 07:34:52 -0300

    changes on app.js/enevelopes.js adding routes.js

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
+const envelopeRouter = require('./routes')
+const cors = require('cors');
+app.use(cors());
 
 
+
+app.get('/', (req,res,next) => {
+    res.send('Hi')
+
+})
 
 app.listen(4001, () => {
     console.log('listening on port 4001')
--- envelopes.js
+++ envelopes.js
@@ -1,3 +1,22 @@
-const envelope = []
+const envelopes = []
+/*For this project I will consider a budget envelope as an object with the next body:
+{
+    "category" : name of the category for the envelope
+    "budget" : budget use for the category
+}
+*/
 
-module.exports = envelope
+const isValidEnvelope = (possibleEnvelope) => {
+    const {category, budget} = possibleEnvelope
+    return typeof category === 'string' && !isNaN(budget)
+
+}
+
+
+const addEnvelope = (envelope) => {
+    if (!envelope){
+        envelopes.push(envelope)
+    }
+}
+
+module.exports = envelopes
